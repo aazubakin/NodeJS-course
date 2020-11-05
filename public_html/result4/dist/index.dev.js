43ae00ea0345fd7a9bddc049516ea48758871681
Aleksandr Zubakin <eldoson@yandex.ru>
2020-11-05 09:14:10 +0500

    feat: result4 init

--- public_html/result4/dist/index.dev.js
+++ public_html/result4/dist/index.dev.js
@@ -1,10 +1,9 @@
 "use strict";
 
-var _http = require("http");
-
-var s = (0, _http.Server)(function (req, res) {
+var s = require('http').Server(function (req, res) {
   //resres.writeHead(200, {'Content-Length': 3});
   res.write('OK\n');
   res.end();
 });
-s.listen();
+
+s.listen(4321);
--- public_html/result4/index.js
+++ public_html/result4/index.js
@@ -1,7 +1,7 @@
-import {Server} from 'http';
-const s = Server((req, res) => {
+
+const s = require('http').Server((req, res) => {
     //resres.writeHead(200, {'Content-Length': 3});
    res.write('OK\n');
-    res.end();
+   res.end();
 });
-s.listen();
+s.listen(4321);
